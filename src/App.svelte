39185ad52b6dd4e8e49a791c0fb906bc2c31fee7
EvickaStudio <kirenogfx@gmail.com>
2024-05-02 09:01:48 +0200

    Formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,80 +1,80 @@
 <script>
-  import { onMount } from "svelte";
-  import "./app.css";
+    import {onMount} from "svelte";
+    import "./app.css";
 
-  onMount(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("animate-on-scroll");
-          }
+    onMount(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("animate-on-scroll");
+                    }
+                });
+            },
+            {
+                threshold: 0.1,
+            }
+        );
+
+        document.querySelectorAll(".project").forEach((el) => {
+            observer.observe(el);
         });
-      },
-      {
-        threshold: 0.1,
-      }
-    );
 
-    document.querySelectorAll(".project").forEach((el) => {
-      observer.observe(el);
+        document.querySelectorAll(".animate-on-scroll").forEach((el) => {
+            observer.observe(el);
+        });
     });
+    export let name, cdn;
 
-    document.querySelectorAll(".animate-on-scroll").forEach((el) => {
-      observer.observe(el);
-    });
-  });
-  export let name, cdn;
-
-  import { projects } from "./Data.js";
+    import {projects} from "./Data.js";
 </script>
 
 <main class="main-content">
-  <div class="content">
-    <img
-      src="{cdn}/static/media/header_logo.svg"
-      alt="Evicka Studio Logo"
-      class="profile-image"
-    />
-    <h1 id="welcome">Hi there 👋, I'm {name}!</h1>
-    <p id="about">
-      I'm a Computer Science and Software Engineering student based in Germany.
-      I have a passion for learning new programming languages, exploring code
-      design principles, and integrating various services and techniques into my
-      projects. My personal projects often involve automation and user interface
-      design. Additionally, I enjoy contributing to and using open-source
-      projects.
-    </p>
-    <!-- Button for my github -->
-    <button>
-      <a href="https://github.com/EvickaStudio" target="_blank">GitHub</a>
-    </button>
-  </div>
-
-  <section id="projects" class="projects-section">
-    <h2>My Projects</h2>
-    <div class="projects-grid">
-      {#each projects as project}
-        <div class="project animate-on-scroll">
-          <img src={project.imageUrl} alt={project.name + " Image"} />
-          <h3>{project.name}</h3>
-          <p>{project.description}</p>
-          <div class="badges">
-            {#each project.badges as badge}
-              <div class="badge">
-                <img
-                  src={badge.icon}
-                  alt={badge.label}
-                  title={badge.label}
-                  class="badge-icon"
-                />
-                <span class="badge-label">{badge.label}</span>
-              </div>
-            {/each}
-          </div>
-          <a href={project.githubUrl} target="_blank">View on GitHub</a>
-        </div>
-      {/each}
+    <div class="content">
+        <img
+                src="{cdn}/static/media/header_logo.svg"
+                alt="Evicka Studio Logo"
+                class="profile-image"
+        />
+        <h1 id="welcome">Hi there 👋, I'm {name}!</h1>
+        <p id="about">
+            I'm a Computer Science and Software Engineering student based in Germany.
+            I have a passion for learning new programming languages, exploring code
+            design principles, and integrating various services and techniques into my
+            projects. My personal projects often involve automation and user interface
+            design. Additionally, I enjoy contributing to and using open-source
+            projects.
+        </p>
+        <!-- Button for my github -->
+        <button>
+            <a href="https://github.com/EvickaStudio" target="_blank">GitHub</a>
+        </button>
     </div>
-  </section>
+
+    <section id="projects" class="projects-section">
+        <h2>My Projects</h2>
+        <div class="projects-grid">
+            {#each projects as project}
+                <div class="project animate-on-scroll">
+                    <img src={project.imageUrl} alt={project.name + " Image"}/>
+                    <h3>{project.name}</h3>
+                    <p>{project.description}</p>
+                    <div class="badges">
+                        {#each project.badges as badge}
+                            <div class="badge">
+                                <img
+                                        src={badge.icon}
+                                        alt={badge.label}
+                                        title={badge.label}
+                                        class="badge-icon"
+                                />
+                                <span class="badge-label">{badge.label}</span>
+                            </div>
+                        {/each}
+                    </div>
+                    <a href={project.githubUrl} target="_blank">View on GitHub</a>
+                </div>
+            {/each}
+        </div>
+    </section>
 </main>
--- src/Data.js
+++ src/Data.js
@@ -1,143 +1,142 @@
-
 export let projects = [
-  {
-    name: "MoodleMate",
-    description:
-      "Moodle-Mate is a Python program designed to fetch Moodle notifications at regular intervals (60 seconds), summarize their content using GPT-3/4, and send it summarized as a notifications via Pushbullet to your smartphone or webhook to Discord. ",
-    imageUrl: "https://cdn.evickastudio.de/static/media/moodemate_cover.png",
-    githubUrl: "https://github.com/EvickaStudio/Moodle-Mate",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-    ],
-  },
-  {
-    name: "ByteFM Downloader",
-    description:
-      "This simple Python script enables you to download the high-quality ByteFM livestream and save it as an MP3 file. ",
-    imageUrl:
-      "https://cdn.evickastudio.de/static/media/bytefm_cover_big.webp",
-    githubUrl: "https://github.com/EvickaStudio/ByteFM-Livestream-Downloader",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-    ],
-  },
-  {
-    name: "24Fire RestAPI",
-    description:
-      "A simple API wrapper for the 24Fire Rest API to control basic functions of a KVM server.",
-    imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
-    githubUrl: "https://github.com/EvickaStudio/24-Fire-REST-API",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-      // {
-      //   icon: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
-      //   label: "PyPI",
-      // },
-    ],
-  },
-  {
-    name: "Evicka Studio",
-    description:
-      "A very simple Cloudflare R2 file explorer build using Python and PyQT6 ",
-    imageUrl:
-      "https://github.com/EvickaStudio/r2-explorer/raw/main/assets/image.png",
-    githubUrl: "https://github.com/EvickaStudio/r2-explorer",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-      // {
-      //   icon: "https://pypi.org/static/images/logo-small.8998e9d1.svg",
-      //   label: "PyPI",
-      // },
-    ],
-  },
-  {
-    name: "Lorem Ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
-    imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
-    githubUrl: "https://github.com/EvickaStudio",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-    ],
-  },
-  {
-    name: "Lorem Ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
-    imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
-    githubUrl: "https://github.com/EvickaStudio",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-    ],
-  },
-  {
-    name: "Lorem Ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
-    imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
-    githubUrl: "https://github.com/EvickaStudio",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-    ],
-  },
-  {
-    name: "Lorem Ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
-    imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
-    githubUrl: "https://github.com/EvickaStudio",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-    ],
-  },
-  {
-    name: "Lorem Ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
-    imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
-    githubUrl: "https://github.com/EvickaStudio",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-    ],
-  },
-  {
-    name: "Lorem Ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
-    imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
-    githubUrl: "https://github.com/EvickaStudio",
-    badges: [
-      {
-        icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
-        label: "Python",
-      },
-    ],
-  },
+    {
+        name: "MoodleMate",
+        description:
+            "Moodle-Mate is a Python program designed to fetch Moodle notifications at regular intervals (60 seconds), summarize their content using GPT-3/4, and send it summarized as a notifications via Pushbullet to your smartphone or webhook to Discord. ",
+        imageUrl: "https://cdn.evickastudio.de/static/media/moodemate_cover.png",
+        githubUrl: "https://github.com/EvickaStudio/Moodle-Mate",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+        ],
+    },
+    {
+        name: "ByteFM Downloader",
+        description:
+            "This simple Python script enables you to download the high-quality ByteFM livestream and save it as an MP3 file. ",
+        imageUrl:
+            "https://cdn.evickastudio.de/static/media/bytefm_cover_big.webp",
+        githubUrl: "https://github.com/EvickaStudio/ByteFM-Livestream-Downloader",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+        ],
+    },
+    {
+        name: "24Fire RestAPI",
+        description:
+            "A simple API wrapper for the 24Fire Rest API to control basic functions of a KVM server.",
+        imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
+        githubUrl: "https://github.com/EvickaStudio/24-Fire-REST-API",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+            // {
+            //   icon: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
+            //   label: "PyPI",
+            // },
+        ],
+    },
+    {
+        name: "Evicka Studio",
+        description:
+            "A very simple Cloudflare R2 file explorer build using Python and PyQT6 ",
+        imageUrl:
+            "https://github.com/EvickaStudio/r2-explorer/raw/main/assets/image.png",
+        githubUrl: "https://github.com/EvickaStudio/r2-explorer",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+            // {
+            //   icon: "https://pypi.org/static/images/logo-small.8998e9d1.svg",
+            //   label: "PyPI",
+            // },
+        ],
+    },
+    {
+        name: "Lorem Ipsum",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
+        imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
+        githubUrl: "https://github.com/EvickaStudio",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+        ],
+    },
+    {
+        name: "Lorem Ipsum",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
+        imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
+        githubUrl: "https://github.com/EvickaStudio",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+        ],
+    },
+    {
+        name: "Lorem Ipsum",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
+        imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
+        githubUrl: "https://github.com/EvickaStudio",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+        ],
+    },
+    {
+        name: "Lorem Ipsum",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
+        imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
+        githubUrl: "https://github.com/EvickaStudio",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+        ],
+    },
+    {
+        name: "Lorem Ipsum",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
+        imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
+        githubUrl: "https://github.com/EvickaStudio",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+        ],
+    },
+    {
+        name: "Lorem Ipsum",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl",
+        imageUrl: "https://cdn.evickastudio.de/static/media/evickastudio_pic.svg",
+        githubUrl: "https://github.com/EvickaStudio",
+        badges: [
+            {
+                icon: "https://cdn.evickastudio.de/static/media/python_icon.svg",
+                label: "Python",
+            },
+        ],
+    },
 ];
