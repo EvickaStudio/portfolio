cac275116ba820e187d34931ec03b6383c43e0ae
EvickaStudio <kirenogfx@gmail.com>
2024-05-04 13:08:32 +0200

    Sort and refactor code

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,51 @@
 <script>
-    import {onMount} from "svelte";
-    import "./app.css";
-    
+    import { onMount } from 'svelte';
+    import './app.css';
 
     onMount(() => {
         const observer = new IntersectionObserver(
-            (entries) => {
-                entries.forEach((entry) => {
+            entries => {
+                entries.forEach(entry => {
                     if (entry.isIntersecting) {
-                        entry.target.classList.add("animate-on-scroll");
+                        entry.target.classList.add('animate-on-scroll');
                     }
                 });
             },
             {
                 threshold: 0.1,
-            }
+            },
         );
 
-        document.querySelectorAll(".project").forEach((el) => {
+        document.querySelectorAll('.project').forEach(el => {
             observer.observe(el);
         });
 
-        document.querySelectorAll(".animate-on-scroll").forEach((el) => {
+        document.querySelectorAll('.animate-on-scroll').forEach(el => {
             observer.observe(el);
         });
     });
     export let name, cdn;
 
-    import {projects} from "./Data.js";
+    import { projects } from './Data.js';
     // import { blogs } from './Data.js';
 </script>
 
 <main class="main-content">
     <div class="content">
         <img
-                src="{cdn}/static/media/header_logo.svg"
-                alt="Evicka Studio Logo"
-                class="profile-image"
+            src="{cdn}/static/media/header_logo.svg"
+            alt="Evicka Studio Logo"
+            class="profile-image"
         />
-        <h1 id="welcome">Hi there 👋, I'm {name}!</h1>
+        <h1 id="welcome">Hello(), I'm</h1>
+        <h42 id="name">{name}</h42>
         <p id="about">
-            I'm a Computer Science and Software Engineering student based in Germany.
-            I have a passion for learning new programming languages, exploring code
-            design principles, and integrating various services and techniques into my
-            projects. My personal projects often involve automation and user interface
-            design. Additionally, I enjoy contributing to and using open-source
-            projects.
+            I'm a Computer Science and Software Engineering student based in
+            Germany. I have a passion for learning new programming languages,
+            exploring code design principles, and integrating various services
+            and techniques into my projects. My personal projects often involve
+            automation and user interface design. Additionally, I enjoy
+            contributing to and using open-source projects.
         </p>
         <!-- Button for my github -->
     </div>
@@ -55,26 +55,42 @@
         <div class="projects-grid">
             {#each projects as project}
                 <div class="project animate-on-scroll">
-                    <img src={project.imageUrl} alt={project.name + " Image"}/>
+                    <img src={project.imageUrl} alt={project.name + ' Image'} />
                     <h3>{project.name}</h3>
                     <p>{project.description}</p>
-                    <p><span class="label">Type:</span> <span class="value">{project.projectType}</span></p>
-                    <p><span class="label">License:</span> <span class="value">{project.license}</span></p>
-                    <p><span class="label">Tags:</span> <span class="value">{project.tags.join(', ')}</span></p>
+                    <p>
+                        <span class="label">Type:</span>
+                        <span class="value">{project.projectType}</span>
+                    </p>
+                    <p>
+                        <span class="label">License:</span>
+                        <span class="value">{project.license}</span>
+                    </p>
+                    <p>
+                        <span class="label">Tags:</span>
+                        <span class="value">{project.tags.join(', ')}</span>
+                    </p>
                     <div class="badges">
                         {#each project.badges as badge}
-                        <div class="badge" style="border-color: {badge.color ? badge.color : '#3b3b3b'};">
+                            <div
+                                class="badge"
+                                style="border-color: {badge.color
+                                    ? badge.color
+                                    : '#3b3b3b'};"
+                            >
                                 <img
-                                        src={badge.icon}
-                                        alt={badge.label}
-                                        title={badge.label}
-                                        class="badge-icon"
+                                    src={badge.icon}
+                                    alt={badge.label}
+                                    title={badge.label}
+                                    class="badge-icon"
                                 />
                                 <span class="badge-label">{badge.label}</span>
                             </div>
                         {/each}
                     </div>
-                    <a href={project.githubUrl} target="_blank">View on GitHub</a>
+                    <a href={project.githubUrl} target="_blank"
+                        >View on GitHub</a
+                    >
                 </div>
             {/each}
         </div>
@@ -98,14 +114,24 @@
         <p>Feel free to reach out through any of the platforms below.</p>
         <ul>
             <!-- <li>Email: <a href="mailto:your-email@example.com">your-email@example.com</a></li> -->
-            <li>GitHub: <a href="https://github.com/EvickaStudio" target="_blank">EvickaStudio</a></li>
+            <li>
+                GitHub: <a
+                    href="https://github.com/EvickaStudio"
+                    target="_blank">EvickaStudio</a
+                >
+            </li>
             <li>Github: evickastudio</li>
             <!-- Add more social links here -->
         </ul>
     </section>
-<!-- 
+    <!-- 
     imclude link in the name like this [EvickaStudio](https://github.com/EvickaStudio). -->
     <footer style="text-align: center;">
-        <p>Designed and developed with ❤️ by <a href="https://github.com/EvickaStudio" target="_blank">EvickaStudio</a></p>
+        <p>
+            Designed and developed with ❤️ by <a
+                href="https://github.com/EvickaStudio"
+                target="_blank">EvickaStudio</a
+            >
+        </p>
     </footer>
 </main>
